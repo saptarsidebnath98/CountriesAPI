ffd9c575648a0f5b9453500f2878765e18f32d6e
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-06-27 15:01:46 +0530

    fethcing data with useEffect

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,10 +1,40 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 export default function Data() {
 
-    const data = fetch('https://restcountries.com/v3.1/all')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => err);
+   const [countries, setCountries] = useState([]);
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(null);
+
+    useEffect(()=> {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(response => {
+            if(!response.ok){
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            setCountries(data);
+            setLoading(false);
+        })
+        .catch(error => {
+            setError(error);
+            setLoading(false);
+        });
+    }, []);
+
+    if(loading) return <div>Loading...</div>
+    if (error) return <div>Error: {error.message}</div>
+
+    console.log(countries[0]);
+    
   return (
-    <p></p>
+    <ul>
+        {countries.map((country) => {
+            <li key={country.name.official}>{country.name.common}</li>
+        })}
+    </ul>
   )
 }
