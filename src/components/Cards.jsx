7a2b91dfe13f746f715cfbf911d1e102fcc1e177
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-06-28 21:47:05 +0530

    format population

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,7 @@
 
 export default function Cards({ img, name, population, region, capital }) {
+
+    const formattedPopulation = population.toLocaleString('en-IN');
     return (
         <div className="w-72 h-96 shadow-xl rounded-md overflow-hidden pb-36">
             <div className="w-full h-48">
@@ -11,7 +13,7 @@ export default function Cards({ img, name, population, region, capital }) {
             </div>
             <div className="flex flex-col px-4 py-4 bg-white h-48 overflow-y-auto">
                 <h2 className="my-2 text-lg font-bold">{name}</h2>
-                <p><b>Population: </b>{population}</p>
+                <p><b>Population: </b>{formattedPopulation}</p>
                 <p><b>Region: </b>{region}</p>
                 <p><b>Capital: </b>{capital}</p>
             </div>
